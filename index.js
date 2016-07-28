2750ee808346beab6c8e3ce0f9e68882b147b74c
Adam Keenan <adam.g.keenan@gmail.com>
2016-07-28 12:55:58 -0400

    Support override function (fixes #1) and loading from array of conifgs

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var path = require('path');
 var extend = require('extend');
 
 function getConfig(configPath, configName) {
-    var filePath = path.join(process.cwd(), configPath, configName);
+    configName || (configName = '');
+    var filePath = path.join(configPath, configName);
+    if (configPath.startsWith('.')) {
+        filePath = './' + filePath;
+    }
     try {
         fs.accessSync(filePath, fs.F_OK);
         return require(filePath);
@@ -14,21 +18,32 @@ function getConfig(configPath, configName) {
     }
 }
 
-function configamajig(configPath) {
+function configamajig(configPath, configOverride) {
     // Default configPath
     configPath || (configPath = './config');
+    configOverride || (configOverride = {});
 
-    // Get default config
-    var defaultConfig = getConfig(configPath, 'default.json');
+    // If an array is passed in for configPath then treat it as a list of
+    // configs to load.
+    if (Object.prototype.toString.call(configPath) === '[object Array]') {
+        var configs = configPath.map(function (path) {
+            return getConfig(path);
+        });
 
-    // Get env config
-    var env = process.env.NODE_ENV || 'development';
-    var envConfig = getConfig(configPath, env + '.json');
+        return extend.apply(null, [true].concat(configs.concat(configOverride)));
+    } else {
+        // Get default config
+        var defaultConfig = getConfig(configPath, 'default.json');
 
-    // Get local overrides
-    var localConfig = getConfig(configPath, 'local.json');
+        // Get env config
+        var env = process.env.NODE_ENV || 'development';
+        var envConfig = getConfig(configPath, env + '.json');
 
-    return extend(true, defaultConfig, envConfig, localConfig);
+        // Get local overrides
+        var localConfig = getConfig(configPath, 'local.json');
+
+        return extend(true, defaultConfig, envConfig, localConfig, configOverride);
+    }
 }
 
 
